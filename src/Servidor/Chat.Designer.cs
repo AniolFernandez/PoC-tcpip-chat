1fcc4d5b9ff236ce470fe65d1e439446f8de0cfb
Aniol Fernandez Cano <37707037+AniolFernandez@users.noreply.github.com>
2021-07-07 19:00:02 +0200

    Upload

---
+++ src/Servidor/Chat.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace Servidor
+{
+    partial class Chat
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textOutput = new System.Windows.Forms.TextBox();
+            this.textInput = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textOutput
+            // 
+            this.textOutput.Location = new System.Drawing.Point(12, 12);
+            this.textOutput.Multiline = true;
+            this.textOutput.Name = "textOutput";
+            this.textOutput.Size = new System.Drawing.Size(367, 298);
+            this.textOutput.TabIndex = 0;
+            // 
+            // textInput
+            // 
+            this.textInput.Location = new System.Drawing.Point(13, 322);
+            this.textInput.Name = "textInput";
+            this.textInput.Size = new System.Drawing.Size(267, 20);
+            this.textInput.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(295, 319);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Enviar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Chat
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(391, 354);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textInput);
+            this.Controls.Add(this.textOutput);
+            this.Name = "Chat";
+            this.Text = "Chat";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textInput;
+        private System.Windows.Forms.Button button1;
+        public System.Windows.Forms.TextBox textOutput;
+    }
+}
